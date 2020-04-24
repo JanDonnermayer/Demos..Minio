14cc1c668e4f96c80d2b657600ac09279614c949
JanDonnermayer <jan@donnermayer.de>
2020-04-24 20:17:42 +0200

    log

--- synchronize.go
+++ synchronize.go
@@ -10,29 +10,31 @@ import (
 
 func synchronize(store1 ObjectStore, store2 ObjectStore) {
 
+	fmt.Println("gathering infos in source...")
+	infosSourceCh := store1.GetInfos()
+
 	fmt.Println("gathering infos in target...")
 	infosTargetCh := store2.GetInfos()
 
-	fmt.Println("gathering infos in source...")
-	infosSourceCh := store1.GetInfos()
+	
+	setSource := set.New()
+	for m := range infosSourceCh {
+		setSource.Insert(m)
+	}
+	fmt.Printf("source: %v infos\n", setSource.Len())
 
 	setTarget := set.New()
 	for m := range infosTargetCh {
 		setTarget.Insert(m)
 	}
-	fmt.Printf("obtained %v infos in target.\n", setTarget.Len())
+	fmt.Printf("target: %v infos\n", setTarget.Len())
 
-	setSource := set.New()
-	for m := range infosSourceCh {
-		setSource.Insert(m)
-	}
-	fmt.Printf("obtained %v infos in source.\n", setSource.Len())
 
 	diffAdd := setSource.Difference(setTarget)
-	fmt.Printf("source except target: %v infos \n", diffAdd.Len())
+	fmt.Printf("source except target: %v infos\n", diffAdd.Len())
 
 	diffSub := setTarget.Difference(setSource)
-	fmt.Printf("target except source: %v infos \n", diffSub.Len())
+	fmt.Printf("target except source: %v infos\n", diffSub.Len())
 	
 	var diffAddInfos []ObjectInfo
 	diffAdd.Do(func(info interface{}) {
